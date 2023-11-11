112faa9d510b0c55f71dd9f2612d09c3ff9c4373
yusunghyun <ysh1513@naver.com>
2023-11-11 18:08:09 +0900

    ✅ chapter 3 optimizing image

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import AcmeLogo from "@/app/ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import styles from "@/app/ui/home.module.css";
-
 import Link from "next/link";
 import { lusitana } from "./ui/fonts";
+import Image from "next/image";
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -30,7 +31,20 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop and mobile versions"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshot of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
