56a5842a27c1bb31ebd5b619c289aaf6ae95a33a
yusunghyun <ysh1513@naver.com>
2023-11-12 21:24:39 +0900

    ✅ chapter13. error-handling

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,7 @@ import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
 import { InvoiceForm } from "@/app/lib/definitions";
+import { notFound } from "next/navigation";
 
 const INITIAL_INVOICE: InvoiceForm = {
   amount: 0,
@@ -17,6 +18,10 @@ export default async function Page({ params }: { params: { id: string } }) {
     fetchCustomers(),
   ]);
 
+  if (!invoice) {
+    notFound();
+  }
+
   return (
     <main>
       <Breadcrumbs
@@ -29,7 +34,7 @@ export default async function Page({ params }: { params: { id: string } }) {
           },
         ]}
       />
-      <Form invoice={invoice || INITIAL_INVOICE} customers={customers} />
+      <Form invoice={invoice} customers={customers} />
     </main>
   );
 }
