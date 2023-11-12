f9a4215344f734740015711789fcd0cc3a162c18
yusunghyun <ysh1513@naver.com>
2023-11-12 23:08:13 +0900

    ✅ chapter16.adding-metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,18 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
 
+import { Metadata } from "next";
+
+// %s 가 동적으로 자식한테서 받아줌
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Acme Dashboard",
+    default: "Acme Dashboard",
+  },
+  description: "The official Next.js Learn Dashboard built with App Router.",
+  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
+
 export default function RootLayout({
   children,
 }: {
